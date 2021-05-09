5285f4c4d0287aeb71d4af6fc762c9cde179dac8
Rishav Das <61372472+rdofficial@users.noreply.github.com>
2021-05-09 09:55:14 +0530

    Removed some errors, added functionality of current working directory and initial directory, added one command like list-files / ls

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 # Importing the required modules
 try:
-	from os import system, path
+	from os import system, path, listdir, remove, chdir, mkdir
 	from sys import platform, argv as arguments
 except Exception as e:
 	# If there are any errors during the importing of the modules, then we display the error on the console screen
@@ -13,9 +13,37 @@ except Exception as e:
 
 class Shell:
 	def __init__(self):
+		# Initially switching to the data/ directory as it is the default location for our shell
+		if path.isdir('data/'):
+			# If the data/ directory exists, then we directly switch to it
+
+			chdir('data/')
+		else:
+			# If the data/ directory does not exists, then we first create it and then switch to it
+
+			mkdir('data/')
+			chdir('data/')
+		
+		# Setting the initial directory and the current working directory
+		self.initialDirectory = path.dirname(path.abspath(__file__))
+		self.currentWorkingDirectory = self.initialDirectory
+
+		# Using a while true loop for infinite runtime of the shell, and would stop only if the user wants so
 		while True:
-			self.shellPrompt = 'wsb-shell~$ '
+			# The default shell prompt (It can be changed during thr runtime, if the user wants to)
+			if self.currentWorkingDirectory == self.initialDirectory:
+				# If the current working directory and the initial directory are same, then we donot display the working directory info on the shell prompt
+
+				self.shellPrompt = 'wsb-shell:~$ '
+			else:
+				# If the current working directory and the initial directory are not same
+
+				self.shellPrompt = f'wsb-shell:{self.currentWorkingDirectory}$'
+
+			# Asking the user to enter a command
 			self.command = input(f'{self.shellPrompt}')
+
+			# Parsing and executing the command
 			token = self.commandParser()
 			self.executeCommand(token)
 
@@ -44,9 +72,37 @@ class Shell:
 
 			pass
 		elif token["command"] == 'exit':
-			# If the user command is to exit the script, then we do it
+			# If the user entered command is to exit the script, then we do it
 
 			exit()
+		elif token["command"] == 'list-files' or token["command"] == 'ls':
+			# If the user entered command is to list the files and folders of the current directory, then we continue
+
+			# Setting a blank variable for directory location
+			directory = ''
+
+			# Checking the user entered arguments
+			if len(token["arguments"]) == 0:
+				# If there are no arguments entered by the user, then we consider the current folder files to be listed
+
+				directory = self.currentWorkingDirectory
+			else:
+				# If there are arguments entered by the user, then we consider the first argument to be the directory location
+
+				directory = token["arguments"][0]
+
+			# Checking if the specified directory exists or not
+			if path.isdir(directory):
+				# If the user specified directory exists, then we continue to list the files
+
+				print(f'\nContents of "{directory} :')
+				for i in listdir(directory):
+					print(f'[*] {i}')
+			else:
+				# If the user specified directory does not exists, then we display an error message on the screen
+
+				print(f'[ Error : No such directory "{directory}" ]')
+				return 0
 		elif token["command"] == 'add':
 			# If the user command is to add, then we continue
 
@@ -60,11 +116,29 @@ class Shell:
 
 				isNumber = False
 				for i in token["arguments"]:
-					if i.isnumeric():
-						isNumber = True
-					else:
+					# if i.isnumeric():
+					# 	isNumber = True
+					# else:
+					# 	isNumber = False
+					# 	break
+
+					# Checking wheter numeric or not
+					try:
+						i = float(i)
+					except ValueError:
+						# If the value error is encountered, it means the argument is not a numeric term
+
 						isNumber = False
 						break
+					except Exception as e:
+						# If there are any other errors encountered during the process, then we display the error on the console screen
+
+						print(f'[ Error : Failed to parse the arguments for the command "{token["command"]}" ]')
+						return 0
+					else:
+						# If there are no errors in the process, then we assume that the argument is a numebr
+
+						isNumber = True
 				if isNumber:
 					# If the arguments are all numbers, then we continue to find out the sum
 
@@ -87,7 +161,6 @@ class Shell:
 						else:
 							open(f'data/{filename}', 'w+').write('')
 							print(f'[ File created : {filename} ]')
-
 		else:
 			# If the user entered command is unrecognized, then we display an error on the console screen
 
